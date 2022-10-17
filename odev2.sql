4a98689adddb484f4cf38165966b9537ed0a4dda
BasriOrak06 <113607525+BasriOrak06@users.noreply.github.com>
2022-10-17 18:42:49 +0300

    Update odev2.sql

--- odev2.sql
+++ odev2.sql
@@ -8,5 +8,5 @@ WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 SELECT first_name, last_name FROM actor
 WHERE first_name IN ('Penelope', 'Nick', 'Ed');
 
-SELECT film_id, title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost,rating,last_update,special_features,fulltext FROM film
+SELECT * FROM film
 WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
